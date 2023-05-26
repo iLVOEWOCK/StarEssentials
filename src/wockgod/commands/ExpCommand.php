086bffedb9113620bfea3ef8c0de71b8eb773a8b
iLVOEWOCK <131495628+iLVOEWOCK@users.noreply.github.com>
2023-05-26 08:28:56 -0700

    Update ExpCommand.php
    Added add subcommand to give experience to players
    Added remove subcommand to remove experience to players

--- src/wockgod/commands/ExpCommand.php
+++ src/wockgod/commands/ExpCommand.php
@@ -5,6 +5,7 @@ namespace wockgod\commands;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
+use pocketmine\Server;
 
 class ExpCommand extends Command
 {
@@ -19,10 +20,62 @@ class ExpCommand extends Command
         if (!$sender instanceof Player) {
             return;
         }
-        $exp = number_format($sender->getXpManager()->getCurrentTotalXp(), 1);
-        $level = $sender->getXpManager()->getXpLevel();
-        $levelup = self::getExpToLevelUp($sender->getXpManager()->getCurrentTotalXp());
-        $sender->sendMessage($sender->getNameTag() . " §r§6has §r§c$exp EXP §r§6(level §r§c" . $level . "§r§6) §r§6and needs $levelup more exp to level up.");
+        if (empty($args)) {
+            $exp = number_format($sender->getXpManager()->getCurrentTotalXp(), 1);
+            $level = $sender->getXpManager()->getXpLevel();
+            $levelup = self::getExpToLevelUp($sender->getXpManager()->getCurrentTotalXp());
+            $sender->sendMessage($sender->getNameTag() . " §r§6has §r§c$exp EXP §r§6(level §r§c" . $level . "§r§6) §r§6and needs $levelup more exp to level up.");
+            return;
+        }
+        switch ($args[0]) {
+            case 'add':
+                if (count($args) !== 3) {
+                    $sender->sendMessage("§cUsage: /xp add <player> <amount>");
+                    return;
+                }
+                $player = Server::getInstance()->getPlayerByPrefix($args[1]);
+                if (!$player) {
+                    $sender->sendMessage("§cPlayer not found.");
+                    return;
+                }
+                $amount = (int) $args[2];
+                if ($amount <= 0) {
+                    $sender->sendMessage("§cAmount must be a positive integer.");
+                    return;
+                }
+                $currentXp = $player->getXpManager()->getCurrentTotalXp();
+                $player->getXpManager()->addXp($amount);
+                $newXp = $player->getXpManager()->getCurrentTotalXp();
+                $sender->sendMessage("§aAdded $amount XP to " . $player->getName() . ". Their new XP is $newXp.");
+                break;
+            case 'remove':
+                if (count($args) !== 3) {
+                    $sender->sendMessage("§cUsage: /xp remove <player> <amount>");
+                    return;
+                }
+                $player = Server::getInstance()->getPlayerByPrefix($args[1]);
+                if (!$player) {
+                    $sender->sendMessage("§cPlayer not found.");
+                    return;
+                }
+                $amount = (int) $args[2];
+                if ($amount <= 0) {
+                    $sender->sendMessage("§cAmount must be a positive integer.");
+                    return;
+                }
+                $currentXp = $player->getXpManager()->getCurrentTotalXp();
+                if ($amount > $currentXp) {
+                    $sender->sendMessage("§c" . $player->getName() . " does not have that much XP.");
+                    return;
+                }
+                $player->getXpManager()->subtractXp($amount);
+                $newXp = $player->getXpManager()->getCurrentTotalXp();
+                $sender->sendMessage("§aRemoved $amount XP from " . $player->getName() . ". Their new XP is $newXp.");
+                break;
+            default:
+                $sender->sendMessage("§cUsage: /xp [add|remove] <player> <amount>");
+                break;
+        }
     }
 
     /**
